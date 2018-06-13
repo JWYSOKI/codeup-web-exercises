184ec44692fc8001ae1f2fbb631251cb75514d39
Jillian Wysoki <jillianwysoki@gmail.com>
2018-06-13 17:00:26 -0500

    Java Function exercises

--- js/external.js
+++ js/external.js
@@ -9,12 +9,48 @@ alert("Welcome to my Website!");
 
 var userFavoriteColor = prompt("What is your favorite color?");
 
-console.log(userFavoriteColor);
 
 alert ("Great, " + userFavoriteColor + " is my favorite color too!");
 
+console.log(userFavoriteColor);
+
+
+var Little_Mermaid_Days_Rented = 3;
+
+
+var Brother_Bear_Days_Rented = 5;
+
+
+var Hercules_Days_Rented = 1;
+
+
+var Movie_Rental_Price_Per_Day = 3;
+
+
+var Question_Little_Mermaid_Days_Rented = prompt("For how many days did you rent The Little Mermaid?");
+
+
+var Question_Brother_Bear_Days_Rented = prompt("For how many days did you rent Brother Bear?");
+
+
+var Question_Hercules_Days_Rented = prompt("For how many days did you rent Hercules?");
+
+
+var Price_for_a_movie_per_day = 3;
+
+// console.log ("Question_Little_Mermaid_Days_Rented") + ("Price_for_a_movie_per-day");
 
 
 
 
 
+
+
+
+
+
+//     Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
+
+// A student can be enrolled to a class only if the class is not full and the class schedule does not conflict with her current schedule.
+//     A product offer can be applied only if people buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
