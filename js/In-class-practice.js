6dc70e0ef819987557952ebf5624531b3844945c
Jillian Wysoki <jillianwysoki@gmail.com>
2018-06-12 21:37:45 -0500

    javascript exercises

---
+++ js/In-class-practice.js
@@ -0,0 +1,34 @@
+
+
+
+//JAVASCRIPT NOTES FROM CLASS
+
+
+// "use strict" must be used inside script tags
+
+"use strict";
+
+console.log("this is my external file loaded");
+
+console.log(1+2);
+
+console.error("Something went wrong");
+
+var age  = 26;
+
+console.log(age);
+
+
+alert();
+// any texts inside the parenthesis will credit an alert window
+
+var answer = confirm("Is it your birthday today?");
+console.log(answer);
+
+var age = prompt("How old are you?");
+
+// Bootstrap
+//     >Modal (a type of window that pops up like an alert or a prompt)
+
+
+
---
+++ js/external.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+console.log ("Hello from External JavaScript");
+
+
+
+alert("Welcome to my Website!");
+
+var userFavoriteColor = prompt("What is your favorite color?");
+
+console.log(userFavoriteColor);
+
+alert ("Great, " + userFavoriteColor + " is my favorite color too!");
+
+
+
+
+
+
