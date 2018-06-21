63e48ae69821bb64360bab76bac520d4bb4fe512
Jillian Wysoki <jillianwysoki@gmail.com>
2018-06-21 12:20:14 -0500

    Manipulating objects

--- js/circle.js
+++ js/circle.js
@@ -1,10 +1,8 @@
 (function() {
     "use strict";
-
     // create a circle object
     var circle = {
         radius: 3,
-
         getArea: function () {
 
             var area =  (Math.PI * (Math.pow(circle.radius,2)));
@@ -15,7 +13,7 @@
         logInfo: function (doRounding) {
             // TODO: complete this method.
 
-            var result = (this.getArea());
+            var result = this.getArea();
 
             if(doRounding) {
                 result = Math.round(this.getArea());
@@ -30,11 +28,13 @@
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
-
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+
     circle.radius = 5;
+
     // log info about the circle
+
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
